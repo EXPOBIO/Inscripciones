3b48c72a82b3b3149c4696b9ec646b2c8788845e
SamBioArt <samthekiller20001726@gmail.com>
2026-06-30 15:15:42 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  NOMBRE_EVENTO: "EXPOBIO 2026",
+  SUBTITULO: "IV Convención Anual de Estudiantes de Biología",
+
+  FACULTAD_DEFAULT: "Ciencias Biológicas",
+
+  API_URL: "AQUI_VA_LA_URL_DE_TU_APPS_SCRIPT",
+
+  UNIVERSIDADES: [
+    "Universidad Nacional de San Antonio Abad del Cusco (UNSAAC)",
+    "Universidad Nacional Intercultural de Quillabamba (UNIQ)",
+    "Universidad Nacional Amazónica de Madre de Dios (UNAMAD)",
+    "Universidad Nacional de San Agustín (UNSA)",
+    "Universidad Nacional del Altiplano (UNAP)",
+    "Universidad Andina del Cusco (UAC)",
+    "Universidad Tecnológica de los Andes (UTEA)"
+  ]
+};
